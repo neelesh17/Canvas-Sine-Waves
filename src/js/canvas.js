de4e22d5a66fa2d3f31cf26cebd19407de65b3c2
Neelesh Thakur <nt00605@gmail.com>
2020-06-25 17:52:46 +0530

    finished customisable sign wave

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,66 @@
-import utils from './utils'
+import * as dat from 'dat.gui';
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const gui = new dat.GUI();
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+const wave = {
+  y: canvas.height/2,
+  length: 0.01,
+  amplitude: 100,
+  frequency: 0.01
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
-
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
-
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
-
-  init()
-})
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
-  }
+const strokeColor = {
+  h: 200,
+  s: 50,
+  l: 50
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
+const rectColor = {
+  r: 0,
+  g: 0,
+  b: 0,
+  a: 0.01
+}
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+
+const waveFolder = gui.addFolder('wave');
+waveFolder.add(wave, 'y', 0, canvas.height);
+waveFolder.add(wave, 'length', -0.01, 0.01);
+waveFolder.add(wave, 'amplitude', -300, 300);
+waveFolder.add(wave, 'frequency', 0.01, 1);
+waveFolder.open();
+
+const strokeFolder = gui.addFolder('strokeColor');
+strokeFolder.add(strokeColor, 'h', 0, 255);
+strokeFolder.add(strokeColor, 's', 0, 100);
+strokeFolder.add(strokeColor, 'l', 0, 100);
+strokeFolder.open();
+
+const rectFolder = gui.addFolder('rectColor');
+rectFolder.add(rectColor, 'r', 0, 255);
+rectFolder.add(rectColor, 'b', 0, 255);
+rectFolder.add(rectColor, 'g', 0, 255);
+rectFolder.add(rectColor, 'a', 0, 0.06);
+rectFolder.open();
+
+let increment = wave.frequency;
+function animate(){
+  requestAnimationFrame(animate);
+  c.fillStyle = `rgba(${rectColor.r}, ${rectColor.g}, ${rectColor.b}, ${rectColor.a})`;
+  c.fillRect(0,0,innerWidth, innerHeight);
+  c.beginPath();
+  c.moveTo(0,canvas.height/2);
+  for(let  i=0;i<canvas.width;i++){
+    c.lineTo(i, wave.y + Math.sin(i * wave.length + increment) * wave.amplitude * Math.sin(1 - increment));
   }
+  c.strokeStyle = `hsl(${strokeColor.h}, ${strokeColor.s}%, ${strokeColor.l}%)`
+  c.stroke();
+  increment += wave.frequency;
 }
 
-// Animation Loop
-function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
-
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
-}
-
-init()
-animate()
+animate();
