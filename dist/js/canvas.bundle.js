17bbed13bbded299b56bb9a80fe2b7895f0de589
Neelesh Thakur <nt00605@gmail.com>
2020-06-27 12:54:46 +0530

    added text

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -2654,13 +2654,13 @@ var strokeColor = {
   h: 200,
   s: 50,
   l: 50
-};
-var rectColor = {
-  r: 0,
-  g: 0,
-  b: 0,
-  a: 0.01
-};
+}; // const rectColor = {
+//   r: 0,
+//   g: 0,
+//   b: 0,
+//   a: 0.01
+// }
+
 var waveFolder = gui.addFolder('wave');
 waveFolder.add(wave, 'y', 0, canvas.height);
 waveFolder.add(wave, 'length', -0.01, 0.01);
@@ -2671,18 +2671,18 @@ var strokeFolder = gui.addFolder('strokeColor');
 strokeFolder.add(strokeColor, 'h', 0, 255);
 strokeFolder.add(strokeColor, 's', 0, 100);
 strokeFolder.add(strokeColor, 'l', 0, 100);
-strokeFolder.open();
-var rectFolder = gui.addFolder('rectColor');
-rectFolder.add(rectColor, 'r', 0, 255);
-rectFolder.add(rectColor, 'b', 0, 255);
-rectFolder.add(rectColor, 'g', 0, 255);
-rectFolder.add(rectColor, 'a', 0, 0.06);
-rectFolder.open();
+strokeFolder.open(); // const rectFolder = gui.addFolder('rectColor');
+// rectFolder.add(rectColor, 'r', 0, 255);
+// rectFolder.add(rectColor, 'b', 0, 255);
+// rectFolder.add(rectColor, 'g', 0, 255);
+// rectFolder.add(rectColor, 'a', 0, 0.06);
+// rectFolder.open();
+
 var increment = wave.frequency;
 
 function animate() {
   requestAnimationFrame(animate);
-  c.fillStyle = "rgba(".concat(rectColor.r, ", ").concat(rectColor.g, ", ").concat(rectColor.b, ", ").concat(rectColor.a, ")");
+  c.fillStyle = "rgba(255, 255, 255, 0.02)";
   c.fillRect(0, 0, innerWidth, innerHeight);
   c.beginPath();
   c.moveTo(0, canvas.height / 2);
@@ -2694,6 +2694,21 @@ function animate() {
   c.strokeStyle = "hsl(".concat(strokeColor.h, ", ").concat(strokeColor.s, "%, ").concat(strokeColor.l, "%)");
   c.stroke();
   increment += wave.frequency;
+  c.font = "bold 60px Balsamiq Sans";
+  c.fillStyle = "rgba(0,0,0,0.5)";
+  c.textAlign = "center";
+  c.textBaseline = "ideographic";
+  var ctext = "SINE WAVES".split("").join(String.fromCharCode(8201));
+  c.fillText(ctext, canvas.width / 2, canvas.height / 2);
+  c.font = "40px Crimson Text";
+  c.fillText("Jul 25, 2020", canvas.width / 2, canvas.height / 2 + 100);
+  c.beginPath();
+  c.moveTo(canvas.width / 2 - 100, canvas.height / 2 + 20);
+  c.lineTo(canvas.width / 2 + 100, canvas.height / 2 + 20);
+  c.strokeStyle = "rgba(0, 0, 0, 0.5)";
+  c.lineWidth = 2.5;
+  c.stroke();
+  c.closePath();
 }
 
 animate();
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -20,12 +20,12 @@ const strokeColor = {
   l: 50
 }
 
-const rectColor = {
-  r: 0,
-  g: 0,
-  b: 0,
-  a: 0.01
-}
+// const rectColor = {
+//   r: 0,
+//   g: 0,
+//   b: 0,
+//   a: 0.01
+// }
 
 
 const waveFolder = gui.addFolder('wave');
@@ -41,17 +41,17 @@ strokeFolder.add(strokeColor, 's', 0, 100);
 strokeFolder.add(strokeColor, 'l', 0, 100);
 strokeFolder.open();
 
-const rectFolder = gui.addFolder('rectColor');
-rectFolder.add(rectColor, 'r', 0, 255);
-rectFolder.add(rectColor, 'b', 0, 255);
-rectFolder.add(rectColor, 'g', 0, 255);
-rectFolder.add(rectColor, 'a', 0, 0.06);
-rectFolder.open();
+// const rectFolder = gui.addFolder('rectColor');
+// rectFolder.add(rectColor, 'r', 0, 255);
+// rectFolder.add(rectColor, 'b', 0, 255);
+// rectFolder.add(rectColor, 'g', 0, 255);
+// rectFolder.add(rectColor, 'a', 0, 0.06);
+// rectFolder.open();
 
 let increment = wave.frequency;
 function animate(){
   requestAnimationFrame(animate);
-  c.fillStyle = `rgba(${rectColor.r}, ${rectColor.g}, ${rectColor.b}, ${rectColor.a})`;
+  c.fillStyle = `rgba(255, 255, 255, 0.02)`;
   c.fillRect(0,0,innerWidth, innerHeight);
   c.beginPath();
   c.moveTo(0,canvas.height/2);
@@ -61,6 +61,21 @@ function animate(){
   c.strokeStyle = `hsl(${strokeColor.h}, ${strokeColor.s}%, ${strokeColor.l}%)`
   c.stroke();
   increment += wave.frequency;
+  c.font = "bold 60px Balsamiq Sans";
+  c.fillStyle = "rgba(0,0,0,0.5)";
+  c.textAlign = "center";
+  c.textBaseline = "ideographic";
+  let ctext = "SINE WAVES".split("").join(String.fromCharCode(8201))
+  c.fillText(ctext, canvas.width/2, canvas.height/2);
+  c.font = "40px Crimson Text";
+  c.fillText("Jul 25, 2020", canvas.width/2, canvas.height/2 + 100); 
+  c.beginPath();
+  c.moveTo(canvas.width/2 - 100 , canvas.height/2 + 20);
+  c.lineTo(canvas.width/2 + 100, canvas.height/2 + 20); 
+  c.strokeStyle = "rgba(0, 0, 0, 0.5)";
+  c.lineWidth = 2.5;
+  c.stroke();
+  c.closePath();
 }
 
 animate();
